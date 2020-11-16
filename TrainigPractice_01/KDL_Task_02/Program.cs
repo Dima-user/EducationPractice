e204c576fb58eb8a94e63b5a49eb09dd75044b9b
Dima-user <62194537+Dima-user@users.noreply.github.com>
2020-11-16 19:36:16 +0300

    Добавил второе задание

---
+++ TrainigPractice_01/KDL_Task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace KDL_Task_02
+{
+    class Program
+    {
+        class Infinity
+        {
+            private static string message;
+
+            public static string Message 
+            {
+                get => message;
+                set => message = value;
+            }
+            public static void Iteration()
+            {
+                Console.WriteLine("Цикл все еще выполняется");
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (Infinity.Message != "exit")
+            {
+                Infinity.Iteration();
+                Infinity.Message = Console.ReadLine();
+            }
+        }
+    }
+}
