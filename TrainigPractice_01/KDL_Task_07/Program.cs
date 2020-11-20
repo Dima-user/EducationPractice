9040574fe0739527a33cdadb1248a23c428166f8
Dima-user <62194537+Dima-user@users.noreply.github.com>
2020-11-20 16:32:07 +0300

    Добавил 7 задание

---
+++ TrainigPractice_01/KDL_Task_07/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KDL_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           char[] mas = new char[7] { 'a', 'b', 'c', 'd','e','f','g' };
+
+           Shuffle<char> shuffle = new Shuffle<char>(mas);
+
+        }
+
+        
+    }
+}
---
+++ TrainigPractice_01/KDL_Task_07/Shuffle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KDL_Task_07
+{
+    class Shuffle<Type>
+    {
+        public Type []  massive;
+        private Random random;
+
+        delegate void ChangingDelegate();
+
+        public delegate void PrintDisplayDelegate();
+       
+
+        public Shuffle(Type [] sortedMassive)
+        {
+            massive = new Type[sortedMassive.Length];
+            Array.Copy(sortedMassive, 0, massive, 0, sortedMassive.Length);
+            ChangingDelegate change = () => ChangeElement();
+            change?.Invoke();
+
+            PrintDisplayDelegate print = () => PrintDisplay();
+            print?.Invoke();
+        }
+
+        private void ChangeElement()
+        {
+            random = new Random();
+
+            for (int i = massive.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+
+                Type temp = massive[j];
+                massive[j] = massive[i];
+                massive[i] = temp;
+            }
+
+        }
+
+        private void PrintDisplay()
+        {
+            for (int iterator = 0; iterator < massive.Length; iterator++)
+            {
+                Console.Write("{0} ",massive[iterator]);
+            }
+        }
+    }
+}
